48348ad957d1ec1654de65e57d0184a029c6fe65
rybakov1 <rybvlad4@gmail.com>
2023-10-25 22:17:13 +0300

    add utils class

---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class utils {
+  static const dark_1 = Color.fromRGBO(68, 68, 68, 100);
+  static const dark_2 = Color.fromRGBO(51, 51, 51, 100);
+  static const dark_3 = Color.fromRGBO(34, 34, 34, 100);
+}
