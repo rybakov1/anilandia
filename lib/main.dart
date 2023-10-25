fbef2d3ee6cec78272b1eb20f078c127215d7611
rybakov1 <rybvlad4@gmail.com>
2023-10-25 22:17:46 +0300

    add ru/en lang

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:anilandia/l10n/locales.dart';
 import 'package:anilandia/views/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +11,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Anilandia',
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
+      supportedLocales: Locales.all,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const ProfilePage(),
     );
   }
