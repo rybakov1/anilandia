b803fdec1126b3f5806ccc551a43daf0717bc42e
rybakov1 <rybvlad4@yandex.ru>
2023-10-27 02:58:55 +0300

    fix color scheme

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class utils {
-  static const dark_1 = Color.fromRGBO(68, 68, 68, 100);
-  static const dark_2 = Color.fromRGBO(51, 51, 51, 100);
-  static const dark_3 = Color.fromRGBO(34, 34, 34, 100);
+  static const dark_1 = Color.fromARGB(255, 68, 68, 68);
+  static const dark_2 = Color.fromARGB(255, 51, 51, 51);
+  static const dark_3 = Color.fromARGB(255, 34, 34, 34);
 }
