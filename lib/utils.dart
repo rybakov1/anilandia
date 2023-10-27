0b030bf0833497998e8eb290066df60e28809512
rybakov1 <rybvlad4@yandex.ru>
2023-10-27 23:41:01 +0300

    update color scheme

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,4 +4,9 @@ class utils {
   static const dark_1 = Color.fromARGB(255, 68, 68, 68);
   static const dark_2 = Color.fromARGB(255, 51, 51, 51);
   static const dark_3 = Color.fromARGB(255, 34, 34, 34);
+
+  static const dividerDark_1 = Color(0xFFAAAAAA);
+  static const dividerDark_2 = Color(0xFF888888);
+  static const dividerDark_3 = Color(0xFF666666);
+
 }
