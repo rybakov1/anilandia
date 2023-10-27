43dc0e10ffc0419b322836b81f5b74d5f8a3eaaf
rybakov1 <rybvlad4@yandex.ru>
2023-10-27 23:43:13 +0300

    block app orientation

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:anilandia/views/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) => {runApp(const MyApp())});
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
